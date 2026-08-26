f65655ccd1d784fc6fe4023c5c58f89b8f47f5fc
sjh5966-spec <sjh5966@gmail.com>
2026-08-26 13:07:11 +0900

    Retry SECDatabase parquet without extension glob

--- test_secdatabase_s3.py
+++ test_secdatabase_s3.py
@@ -4,8 +4,14 @@ con.execute("INSTALL httpfs; LOAD httpfs")
 con.execute("SET s3_region='us-east-1'")
 con.execute("SET s3_url_style='path'")
 con.execute("SET s3_endpoint='s3.amazonaws.com'")
-path='s3://dataset.secdatabase.com/sec_financial_statements/parquet/20200930/data_point/*.parquet'
-try:
-    print(con.execute(f"SELECT COUNT(*),MIN(filing_date),MAX(filing_date) FROM read_parquet('{path}')").fetchone())
-except Exception as e:
-    print(type(e).__name__,repr(e));sys.exit(1)
+for path in [
+ 's3://dataset.secdatabase.com/sec_financial_statements/parquet/20200930/data_point/*',
+ 'https://dataset.secdatabase.com.s3.amazonaws.com/sec_financial_statements/parquet/20200930/data_point/*'
+]:
+    try:
+        print('TRY',path)
+        print(con.execute(f"SELECT COUNT(*),MIN(filing_date),MAX(filing_date) FROM read_parquet('{path}')").fetchone())
+        sys.exit(0)
+    except Exception as e:
+        print(type(e).__name__,repr(e))
+sys.exit(1)
